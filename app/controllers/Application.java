ae3abcd4037231a702824eaa4350bdd0014b9c9b
carol <cxnvcarol@gmail.com>
2015-04-02 04:50:08 -0500

    html dummies

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,15 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());//"Hybrid recommender system"));
+    }
+
+    public static Result results() {
+        return ok(recommender.render());//"Hybrid recommender system"));
+    }
+
+    public static Result evaluation() {
+        return ok(evaluation.render());//"Hybrid recommender system"));
     }
 
 }
