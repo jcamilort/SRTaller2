c5b454ecae0efc71697441b9cddf425c91014923
Juan Camilo Ortiz <juanco.ortizr@gmail.com>
2015-04-04 12:13:39 -0500

    Checkins Loading complete

--- app/recommender/DataLoader.java
+++ app/recommender/DataLoader.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import models.Business;
 import models.Checkin;
@@ -20,10 +23,13 @@ import org.json.simple.parser.ParseException;
 public class DataLoader {
 
 	public static final String rutaUsuarios = "./data/yelp_academic_dataset_user.json";
-	public static final String rutaUsuariosTest = "./data/yelp_academic_dataset_user_test.json";
+	public static final String rutaTips = "./data/yelp_academic_dataset_tip.json";
+	public static final String rutaNegocios = "./data/yelp_academic_dataset_business.json";
 	public static EntityCollector colector;
 	public static final String rutaNegociosTest = "./data/yelp_academic_dataset_business_test.json";
-
+	public static final String rutaCheckinsTest = "./data/yelp_academic_dataset_checkins_test.json";
+	public static final String rutaUsuariosTest = "./data/yelp_academic_dataset_user_test.json";
+	public static final String rutaArchivoDataModel = "./data/reviews_info.txt";
 	public static void main(String[] args0) {
 		colector = EntityCollector.getInstance();
 		cargarNegocios();
@@ -31,36 +37,64 @@ public class DataLoader {
 		cargarCheckins();
 		cargarReviews();
 		cargarTips();
-		
+
 		System.out.println("Carga Completa");
 	}
-	
+
 	/**
 	 * Genera el archivo que alimenta el recomendador de la libreria
 	 */
 	private static void generarArchivoDataModel() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private static void cargarReviews() {
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(rutaUsuariosTest));
+			BufferedReader br = new BufferedReader(new FileReader(
+					rutaUsuariosTest));
 			System.out.println("Lee el archivo");
 			String line = "";
-			Review review;
+			
+			PrintWriter writer = new PrintWriter(rutaArchivoDataModel, "UTF-8");
+			
 			while ((line = br.readLine()) != null) {
 
-				line = line.replace("\\", "");
-				System.out.println("Nueva linea");
-				System.out.println(line);
+				try {
+					Review review = new Review();
+					line = line.replace("\\", "");
+					System.out.println("Nueva linea");
+					System.out.println(line);
 
-				JSONParser jsonParser = new JSONParser();
-				JSONObject jsonObject = (JSONObject) jsonParser.parse(line);
-				System.out.println(jsonObject.toJSONString());
+					JSONParser jsonParser = new JSONParser();
+					JSONObject jsonObject = (JSONObject) jsonParser.parse(line);
+					System.out.println(jsonObject.toJSONString());
 
+					String business_id = (String) jsonObject.get("business_id");
+					String user_id = (String) jsonObject.get("user_id");
+					long stars_long = (Long) jsonObject.get("stars");
+					double stars = (double) stars_long;
+					String text = (String) jsonObject.get("text");
+					Date d = (new SimpleDateFormat("yyyy-MM-dd"))
+							.parse(jsonObject.get("date").toString());
+					// TODO extraer votes
+
+					review.setBusiness_id(business_id);
+					review.setDate(d);
+					review.setStars(stars);
+					review.setText(text);
+					review.setUser_id(user_id);
+					review.save();
+					
+					writer.println(business_id+"	"+user_id+"		"+stars);
+
+				} catch (java.text.ParseException e) {
+					e.printStackTrace();
+				}
 			}
-			generarArchivoDataModel();
+			
+			writer.close();
+			
 		} catch (FileNotFoundException e) {
 			System.out.println("Error loading users: file not found.");
 			e.printStackTrace();
@@ -75,40 +109,59 @@ public class DataLoader {
 
 	private static void cargarTips() {
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(rutaUsuariosTest));
+			BufferedReader br = new BufferedReader(new FileReader(rutaTips));
 			System.out.println("Lee el archivo");
 			String line = "";
-			Tip tip;
+			JSONParser jsonParser = new JSONParser();
+			int badParsed = 0;
 			while ((line = br.readLine()) != null) {
 
 				line = line.replace("\\", "");
-				System.out.println("Nueva linea");
-				System.out.println(line);
 
-				JSONParser jsonParser = new JSONParser();
-				JSONObject jsonObject = (JSONObject) jsonParser.parse(line);
-				System.out.println(jsonObject.toJSONString());
+				System.out.println("nl_tips");
+				try {
+					JSONObject jsonObject = (JSONObject) jsonParser.parse(line);
+					Tip t = new Tip();
+					int likes = Integer.parseInt(jsonObject.get("likes")
+							.toString());
+					Date d = (new SimpleDateFormat("yyyy-MM-dd"))
+							.parse(jsonObject.get("date").toString());
+					String te = jsonObject.get("text").toString();
+					String ui = jsonObject.get("user_id").toString();
+					String bi = jsonObject.get("business_id").toString();
 
+					t.setBusinees_id(bi.toString());
+					t.setUserID(ui);
+					t.setLikes(likes);
+					t.setDate(d);
+					t.setText(te);
+					t.save();
+				} catch (ParseException e) {
+					e.printStackTrace();
+					badParsed++;
+				} catch (java.text.ParseException e) {
+					e.printStackTrace();
+				}
 			}
+			System.out.println("Bad parsed: " + badParsed);
+			br.close();
+
 		} catch (FileNotFoundException e) {
-			System.out.println("Error loading users: file not found.");
 			e.printStackTrace();
 		} catch (IOException e) {
-			System.out.println("IO Exception: " + e.getMessage());
-			e.printStackTrace();
-		} catch (ParseException e) {
-			System.out.println("Parse Exception: " + e.getMessage());
 			e.printStackTrace();
 		}
+
 	}
 
 	private static void cargarCheckins() {
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(rutaUsuariosTest));
+			BufferedReader br = new BufferedReader(new FileReader(
+					rutaCheckinsTest));
 			System.out.println("Lee el archivo");
 			String line = "";
-			Checkin checkin;
 			while ((line = br.readLine()) != null) {
+				Checkin checkin = new Checkin();
 
 				line = line.replace("\\", "");
 				System.out.println("Nueva linea");
@@ -117,6 +170,28 @@ public class DataLoader {
 				JSONParser jsonParser = new JSONParser();
 				JSONObject jsonObject = (JSONObject) jsonParser.parse(line);
 				System.out.println(jsonObject.toJSONString());
+
+				JSONObject structure = (JSONObject) jsonObject
+						.get("checkin_info");
+				String business_id = (String) jsonObject.get("business_id");
+
+				checkin.setBusiness_id(business_id);
+
+				// Extracts checkins
+				for (int i = 0; i < 24; i++) {
+					ArrayList<Integer> byDays = new ArrayList<Integer>(7);
+					for (int j = 0; j < 7; j++) {
+						Long checkin_numb = (Long) structure.get(i + "-" + j);
+
+						if (checkin_numb != null) {
+							checkin_numb = (long) 0;
+						}
+						int checkin_numb_int = checkin_numb.intValue();
+						byDays.add(j, checkin_numb_int);
+					}
+					checkin.checkin_info.add(i, byDays);
+
+				}
 
 			}
 		} catch (FileNotFoundException e) {
@@ -133,7 +208,8 @@ public class DataLoader {
 
 	private static void cargarUsuarios() {
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(rutaUsuariosTest));
+			BufferedReader br = new BufferedReader(new FileReader(
+					rutaUsuariosTest));
 			System.out.println("Lee el archivo");
 			String line = "";
 			User usuario;
@@ -152,9 +228,9 @@ public class DataLoader {
 				String name = (String) jsonObject.get("name");
 				System.out.println("User's name is: " + name);
 				String user_id = (String) jsonObject.get("user_id");
-				long review_count_long = (Long)jsonObject.get("review_count");
+				long review_count_long = (Long) jsonObject.get("review_count");
 				int review_count = (int) review_count_long;
-				
+
 				long fans = (Long) jsonObject.get("fans");
 				int fans_int = (int) fans;
 				double average_stars = (Double) jsonObject.get("average_stars");
@@ -179,6 +255,7 @@ public class DataLoader {
 				usuario.setAverage_stars(average_stars);
 				usuario.setFans(fans_int);
 				usuario.setFriends(amigos);
+				usuario.save();
 
 				colector.addUser(usuario);
 			}
@@ -262,5 +339,4 @@ public class DataLoader {
 			e.printStackTrace();
 		}
 	}
-
 }
